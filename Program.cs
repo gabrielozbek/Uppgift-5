43e1757fdef689e5aeab6bed7a4ee18653b47272
gabrielozbek <gabriello14_@hotmail.com>
2024-10-03 09:49:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Kalkylator");
+        Console.WriteLine("Välj en operation (+, -, *, /):");
+        char operation = Console.ReadKey().KeyChar;
+
+        Console.WriteLine("\nAnge första talet:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ange andra talet:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Fel: Ogiltig operation.");
+                return;
+        }
+
+        Console.WriteLine($"Resultat: {result}");
+        Console.ReadKey();
+    }
+}
