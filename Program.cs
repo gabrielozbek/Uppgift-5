9ef9d57258ca44792bbe9509a3487ea7723b16ea
gabrielozbek <gabriello14_@hotmail.com>
2024-10-03 12:34:22 +0200

    Skapar klass Kalkyleringar Skapar klass kalkyleringar där jag skapar metoder för addition, substraction, multiplication & divsion.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,43 @@
-﻿internal class Program
+﻿
+
+
+public class Kaklkyleringar
+{
+    public void addition()
+    {
+
+    }
+
+    public void substraction()
+    {
+
+    }
+
+    public void multiplication()
+    {
+
+    }
+
+    public void division()
+    {
+
+    }
+}
+public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Kalkylator");
-        Console.WriteLine("Välj en operation (+, -, *, /):");
+        Console.WriteLine("Enkel Kalkylator");
+        Console.WriteLine("Ange operation (+, -, *, /): ");
         char operation = Console.ReadKey().KeyChar;
 
-        Console.WriteLine("\nAnge första talet:");
+        Console.WriteLine("\nAnge första talet: ");
         double num1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Ange andra talet:");
+        Console.WriteLine("Ange andra talet: ");
         double num2 = Convert.ToDouble(Console.ReadLine());
 
-        double result;
+        double result = 0;
 
         switch (operation)
         {
@@ -29,12 +54,48 @@
                 result = num1 / num2;
                 break;
             default:
-                Console.WriteLine("Fel: Ogiltig operation.");
+                Console.WriteLine("Ogiltig operation.");
                 return;
+                switch (operation)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fel: Division med noll är inte tillåten.");
+                            return;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Ogiltig operation.");
+                        return;
+                }
+                Console.WriteLine($"Resultat: {result}");
+                Console.ReadKey();
         }
 
         Console.WriteLine($"Resultat: {result}");
         Console.ReadKey();
-        //test
+        static double LäsInTal()
+        {
+            double tal;
+            while (!double.TryParse(Console.ReadLine(), out tal))
+            {
+                Console.WriteLine("Felaktig inmatning, ange ett giltigt tal:");
+            }
+            return tal;
+        }
     }
 }
